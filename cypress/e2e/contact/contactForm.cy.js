4a1d93b35c904f1c9a29c9116c5f7f0e03295674
BustamanteMF <malena_b_@hotmail.com.ar>
2026-07-28 18:27:40 -0300

    test(contact): automatización del caso negativo de envío de mensaje con mail sin formato y con su fixture correspondiente

--- cypress/e2e/contact/contactForm.cy.js
+++ cypress/e2e/contact/contactForm.cy.js
@@ -21,4 +21,22 @@ describe('Contact Form', () => {
       cy.reload()
     })
   })
+
+  it('TC-CON-02 - Envío de mensaje con formato de mail inválido', () => {
+    cy.fixture('contactData').then((datos) => {
+      const valid = datos.validMessage
+      const invalid = datos.invalidMessage
+      cy.get('[data-testid="ContactName"]').type(valid.name)
+      cy.get('[data-testid="ContactEmail"]').type(invalid.email)
+      cy.get('[data-testid="ContactPhone"]').type(valid.phone)
+      cy.get('[data-testid="ContactSubject"]').type(valid.subject)
+      cy.get('[data-testid="ContactDescription"]').type(valid.description)
+      cy.intercept('POST', '/api/message').as('postMessageRequest')
+      cy.get('.d-grid > .btn').click()
+      cy.wait('@postMessageRequest').its('response.statusCode').should('eq', 400)
+      cy.get('#contact').should('not.contain', 'Thanks for getting in touch '+ valid.name)
+      cy.get('.alert').should('contain', 'must be a well-formed email address')
+      cy.reload()
+    })
+  })
 })
